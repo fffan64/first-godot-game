dd00ae01aea3daa048a5b1ec4895ecfcee34ab94
fffan64 <ff.fan@hotmail.fr>
2024-05-02 23:56:21 +0200

    feat: add blue potion and double jump

--- game_src/scripts/game_manager.gd
+++ game_src/scripts/game_manager.gd
@@ -2,7 +2,14 @@ extends Node
 
 var score = 0
 @onready var score_label = $ScoreLabel
+@onready var double_jump_icon = %"DoubleJump icon"
+
+var hasDoubleJump = false
 
 func add_point():
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins."
+
+func add_potion_blue():
+	double_jump_icon.visible = true
+	hasDoubleJump = true
--- game_src/scripts/player.gd
+++ game_src/scripts/player.gd
@@ -4,9 +4,13 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+var jump_count = 0
+var max_jumps = 2
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var game_manager = %GameManager
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var jump_sound = $JumpSound
 @onready var walk_sound = $WalkSound
@@ -18,10 +22,20 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	if is_on_floor():
+		jump_count = 0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	
+	if game_manager.hasDoubleJump == true:
+		max_jumps = 2
+	else:
+		max_jumps = 1
+	
+	if Input.is_action_just_pressed("jump") and jump_count < max_jumps:
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
 		jump_sound.play()
 
 	# Get the input direction: -1, 0, 1
