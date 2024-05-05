ce10279fefea0b8c78a67620abdbf676222fcf72
fffan64 <ff.fan@hotmail.fr>
2024-05-05 14:12:36 +0200

    feat: walljump + refacto

--- game_src/scripts/player.gd
+++ game_src/scripts/player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 var DASH_SPEED = 260.0
 var dashing = false
 var can_dash = true
@@ -11,8 +14,11 @@ var can_dash = true
 var jump_count = 0
 var max_jumps = 2
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var wall_jump_pushback = 800
+var wall_slide_gravity = 50
+
+var is_wall_sliding_left = false
+var is_wall_sliding_right = false
 
 @onready var game_manager = %GameManager
 @onready var animated_sprite = $AnimatedSprite2D
@@ -25,11 +31,49 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var canPlayWalkSound = true
 
 func _physics_process(delta):
+	var input_dir: Vector2 = input()
 	
-	if Input.is_action_just_pressed("shaderVHS"):
-		retro_vhs_postprocess_fx.visible = not retro_vhs_postprocess_fx.visible
+	others()
+	move(input_dir)
+	jump(delta)
+	wall_slide(delta)
+	dash(input_dir)
+	animate(input_dir)
+
+
+func input() -> Vector2:
+	var input_dir = Vector2.ZERO
+	input_dir.x = Input.get_axis("move_left", "move_right")
+	input_dir = input_dir.normalized()
+	return input_dir
+
+func move(direction):
+	var velocityCalc = 0
+	if dashing:
+		velocityCalc = direction.x * DASH_SPEED
+		#velocity.y = -10
+	else:
+		velocityCalc = direction.x * SPEED
 	
+	if is_wall_sliding_left:
+		if direction.x > 0:
+			velocity.x = velocityCalc
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	if is_wall_sliding_right:
+		if direction.x < 0:
+			velocity.x = velocityCalc
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 	
+	if !is_wall_sliding_left and !is_wall_sliding_right:
+		if direction.x:
+			velocity.x = velocityCalc
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+
+func jump(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -43,33 +87,65 @@ func _physics_process(delta):
 	else:
 		max_jumps = 1
 	
-	if Input.is_action_just_pressed("jump") and jump_count < max_jumps:
-		velocity.y = JUMP_VELOCITY
-		jump_count += 1
-		jump_sound.play()
+	if Input.is_action_just_pressed("jump"):
+		
+		if jump_count < max_jumps and !is_on_wall():
+			velocity.y = JUMP_VELOCITY
+			jump_count += 1
+			jump_sound.play()
+			createJumpParticles(position)
+			
+		
+		if is_on_wall() and Input.is_action_pressed("move_right"):
+			velocity.y = JUMP_VELOCITY
+			velocity.x = -wall_jump_pushback
+			jump_sound.play()
+			createJumpParticles(position)
+		if is_on_wall() and Input.is_action_pressed("move_left"):
+			velocity.y = JUMP_VELOCITY
+			velocity.x = wall_jump_pushback
+			jump_sound.play()
+			createJumpParticles(position)
 
-	# Get the input direction: -1, 0, 1
-	var direction = Input.get_axis("move_left", "move_right")
+func wall_slide(delta):
+	if is_on_wall() and !is_on_floor():
+		if Input.is_action_pressed("move_left"):
+			is_wall_sliding_left = true
+		else:
+			is_wall_sliding_left = false
+		if Input.is_action_pressed("move_right"):
+			is_wall_sliding_right = true
+		else:
+			is_wall_sliding_right = false
+	else:
+		is_wall_sliding_left = false
+		is_wall_sliding_right = false
 	
+	if is_wall_sliding_left or is_wall_sliding_right:
+		velocity.y += (wall_slide_gravity * delta)
+		velocity.y = min(velocity.y, wall_slide_gravity)
+
+func dash(direction):
 	# Dash
-	if game_manager.hasDash and Input.is_action_just_pressed("dash") and can_dash and direction != 0:
+	if game_manager.hasDash and Input.is_action_just_pressed("dash") and can_dash and direction.x != 0:
 		dashing = true
 		can_dash = false
 		dash_sound.play()
 		$Dash_timer.start()
 		$Dash_timer_again.start()
-	
+
+func animate(direction):
 	# Flip the Sprite
-	if direction > 0:
+	if direction.x > 0:
 		animated_sprite.flip_h = false
-	elif direction < 0:
+	elif direction.x < 0:
 		animated_sprite.flip_h = true
 		
 	# Play animations
 	if dashing:
 		animated_sprite.play("dash")
 	elif is_on_floor():
-		if direction == 0:
+		if direction.x == 0:
 			animated_sprite.play("idle")
 		else:
 			animated_sprite.play("run")
@@ -81,20 +157,16 @@ func _physics_process(delta):
 				timer.start()
 	else:
 		animated_sprite.play("jump")
-	
-	
-	if direction:
-		if dashing:
-			velocity.x = direction * DASH_SPEED
-			#velocity.y = -10
-		else:
-			velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 
-	move_and_slide()
+func createJumpParticles(pos):
+	var inst = preload("res://scenes/explosion_particles.tscn").instantiate()
+	inst.position = pos
+	get_parent().add_child(inst)
 
-
+func others():
+	if Input.is_action_just_pressed("shaderVHS"):
+		retro_vhs_postprocess_fx.visible = not retro_vhs_postprocess_fx.visible
 
 func _on_timer_timeout():
 	canPlayWalkSound = true
