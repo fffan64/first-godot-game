3b8ad0712f30633ccfdf8b3fbe28136c2d698f9c
fffan64 <ff.fan@hotmail.fr>
2024-05-08 10:50:54 +0200

    feat: add exploding block

---
+++ game_src/scripts/breakable_block.gd
@@ -0,0 +1,30 @@
+@tool
+extends Area2D
+
+enum BlockColor {BROWN, LIGHT_BROWN, LIGHT_RED, GREY}
+@export var block_color: BlockColor
+
+func _ready():
+	if block_color == BlockColor.BROWN:
+		$Sprite2D.frame = 1
+	elif block_color == BlockColor.LIGHT_BROWN:
+		$Sprite2D.frame = 3
+	elif block_color == BlockColor.LIGHT_RED:
+		$Sprite2D.frame = 5
+	elif block_color == BlockColor.GREY:
+		$Sprite2D.frame = 8
+	else:
+		$Sprite2D.frame = 1
+
+	
+func _on_body_entered(body):
+	if not Engine.is_editor_hint():
+		$Trigger.queue_free()
+		$Sprite2D.queue_free()
+		$StaticBody2D.queue_free()
+		destroy_block()
+		
+func destroy_block():
+	$BlockSound.play()
+	$AnimationPlayer.play("destroy")
+	
