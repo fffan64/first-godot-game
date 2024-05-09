4c0933bb9a6f149dabe050a2278b01e31c91bfb1
fffan64 <ff.fan@hotmail.fr>
2024-05-09 18:20:52 +0200

    feat: world and level select screens

---
+++ game_src/scripts/world_icon.gd
@@ -0,0 +1,14 @@
+@tool
+extends Control
+
+@export var world_index: int = 1
+@export var level_select_packed: PackedScene = load("res://scenes/world_select/level_select.tscn")
+@onready var level_select_scene: LevelSelect = level_select_packed.instantiate()
+
+func _ready():
+	$Label.text = "World " + str(world_index)
+
+
+func _process(delta):
+	if Engine.is_editor_hint():
+		$Label.text = "World " + str(world_index)
