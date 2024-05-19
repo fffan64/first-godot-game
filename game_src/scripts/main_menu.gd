d3eabe9d02b43ceabdf71b553d16b9fbac4c5f31
fffan64 <ff.fan@hotmail.fr>
2024-05-19 12:36:50 +0200

    feat: prettier main menu

--- game_src/scripts/main_menu.gd
+++ game_src/scripts/main_menu.gd
@@ -1,13 +1,21 @@
 extends MarginContainer
 
-@onready var selector1 = $CenterContainer/VBoxContainer/CenterContainer2/Control/VBoxContainer/CenterContainer/HBoxContainer/Selector
-@onready var selector2 = $CenterContainer/VBoxContainer/CenterContainer2/Control/VBoxContainer/CenterContainer2/HBoxContainer/Selector
-@onready var selector3 = $CenterContainer/VBoxContainer/CenterContainer2/Control/VBoxContainer/CenterContainer3/HBoxContainer/Selector
+@onready var selector1 = $CenterContainer/VBoxContainer/Control/CenterContainer2/Control/VBoxContainer/CenterContainer/HBoxContainer/Selector
+@onready var selector2 = $CenterContainer/VBoxContainer/Control/CenterContainer2/Control/VBoxContainer/CenterContainer2/HBoxContainer/Selector
+@onready var selector3 = $CenterContainer/VBoxContainer/Control/CenterContainer2/Control/VBoxContainer/CenterContainer3/HBoxContainer/Selector
+
+@onready var label_start = $CenterContainer/VBoxContainer/Control/CenterContainer2/Control/VBoxContainer/CenterContainer/HBoxContainer/Control2/Label_Start
+@onready var label_options = $CenterContainer/VBoxContainer/Control/CenterContainer2/Control/VBoxContainer/CenterContainer2/HBoxContainer/Control2/Label_Options
+@onready var label_quit = $CenterContainer/VBoxContainer/Control/CenterContainer2/Control/VBoxContainer/CenterContainer3/HBoxContainer/Control2/Label_Quit
+
+@onready var animation_player = $AnimationPlayer
+@onready var select_sound = $SelectSound
 
 var current_selection = 0
 
 func _ready():
 	set_current_selection(0)
+	animation_player.play("intro")
 	
 func _input(event):
 	if event.is_action_pressed("move_down") and current_selection < 2:
@@ -33,9 +41,23 @@ func set_current_selection(_current_selection):
 	selector1.text = ""
 	selector2.text = ""
 	selector3.text = ""
+	(selector3.get_parent() as HBoxContainer).modulate = Color.WHITE
+	(selector2.get_parent() as HBoxContainer).modulate = Color.WHITE
+	(selector1.get_parent() as HBoxContainer).modulate = Color.WHITE
+	label_start.text = "START"
+	label_options.text = "OPTIONS"
+	label_quit.text = "QUIT"
 	if current_selection == 0:
 		selector1.text = ">"
+		(selector1.get_parent() as HBoxContainer).modulate = Color.DARK_ORANGE
+		label_start.text = "[center][wave amp=50.0 freq=5.0 connected=1]START[/wave][/center]"
 	elif current_selection == 1:
 		selector2.text = ">"
+		(selector2.get_parent() as HBoxContainer).modulate = Color.DARK_ORANGE
+		label_options.text = "[center][wave amp=50.0 freq=5.0 connected=1]OPTIONS[/wave][/center]"
 	elif current_selection == 2:
 		selector3.text = ">"
+		(selector3.get_parent() as HBoxContainer).modulate = Color.DARK_ORANGE
+		label_quit.text = "[center][wave amp=50.0 freq=5.0 connected=1]QUIT[/wave][/center]"
+	select_sound.play()
+	
